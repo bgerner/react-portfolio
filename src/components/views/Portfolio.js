d2c1268b6c1599af80a3297dd6acdb18bf52db98
Code Wolf <brgerner@gmail.com>
2021-07-29 14:47:19 -0700

    fix media query @ 727

--- src/components/views/Portfolio.js
+++ src/components/views/Portfolio.js
@@ -1,17 +1,17 @@
 import React from "react";
-import '../assets/css/app.css';
-import gerner from '../assets/imgs/gerner-fitness.png';
-import coolcali from '../assets/imgs/cool-california.png';
-import techshare from '../assets/imgs/tech-share.png';
-import wildthoughts from '../assets/imgs/wild-thoughts.png';
-import readme from '../assets/imgs/readme-generator.png';
-import portfolio from '../assets/imgs/portfolio.png';
+import "../assets/css/app.css";
+import gerner from "../assets/imgs/gerner-fitness-2.png";
+import coolcali from "../assets/imgs/cool-california.png";
+import techshare from "../assets/imgs/tech-share.png";
+import wildthoughts from "../assets/imgs/wild-thoughts.png";
+import readme from "../assets/imgs/readme-generator.png";
+import portfolio from "../assets/imgs/portfolio.png";
 
 function Portfolio() {
   return (
     <section id="work" className="s">
       <div className="section-title">
-        <h3 id="work-title">Work</h3>
+        <h3>Work</h3>
       </div>
 
       <div className="applications">
@@ -22,7 +22,11 @@ function Portfolio() {
               target="_blank"
               rel="noreferrer"
             >
-              <img id="application1" src={gerner} alt="tech share"/>
+              <div className="img-container">
+                <img class="application" 
+                src={gerner} 
+                alt="tech share" />
+              </div>
               <figcaption>Gerner Fitness</figcaption>
             </a>
           </figure>
@@ -32,11 +36,13 @@ function Portfolio() {
               target="_blank"
               rel="noreferrer"
             >
-              <img
-                className="application"
-                src={coolcali}
-                alt="cool california"
-              />
+              <div className="img-container">
+                <img
+                  className="application"
+                  src={coolcali}
+                  alt="cool california"
+                />
+              </div>
               <figcaption>Cool California</figcaption>
             </a>
           </figure>
@@ -48,11 +54,12 @@ function Portfolio() {
               target="_blank"
               rel="noreferrer"
             >
-              <img
-                className="application"
-                src={techshare}
-                alt="tech share"
-              />
+              <div className="img-container">
+                <img 
+                className="application" 
+                src={techshare} 
+                alt="tech share" />
+              </div>
               <figcaption>Tech Share</figcaption>
             </a>
           </figure>
@@ -62,11 +69,13 @@ function Portfolio() {
               target="_blank"
               rel="noreferrer"
             >
-              <img
-                className="application"
-                src={wildthoughts}
-                alt="wild thoughts"
-              />
+              <div className="img-container">
+                <img
+                  className="application"
+                  src={wildthoughts}
+                  alt="wild thoughts"
+                />
+              </div>
               <figcaption>Wild Thoughts</figcaption>
             </a>
           </figure>
@@ -78,7 +87,9 @@ function Portfolio() {
               target="_blank"
               rel="noreferrer"
             >
-              <img id="application1" src={readme} alt="readme-generator"/>
+              <div className="img-container">
+                <img class="application" src={readme} alt="readme-generator" />
+              </div>
               <figcaption>ReadMe Generator</figcaption>
             </a>
           </figure>
@@ -88,11 +99,13 @@ function Portfolio() {
               target="_blank"
               rel="noreferrer"
             >
-              <img
-                className="application"
-                src={portfolio}
-                alt="react portfolio"
-              />
+              <div className="img-container">
+                <img
+                  className="application"
+                  src={portfolio}
+                  alt="react portfolio"
+                />
+              </div>
               <figcaption>React Portfolio (this!)</figcaption>
             </a>
           </figure>
